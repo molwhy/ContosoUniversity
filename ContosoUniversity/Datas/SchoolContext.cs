32d9112d792a547c72ad5840434518cd9d1b07b9
molwhy <molw_smartcloud@hotmail.com>
2022-01-21 14:03:30 +0800

    增加实体

---
+++ ContosoUniversity/Datas/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Datas
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Students");
+            modelBuilder.Entity<Course>().ToTable("Courses");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollments");
+        }
+    }
+}
